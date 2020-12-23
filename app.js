7c2d286506332a00f19ef1fa8252f39f5751abf7
AhmadAbuZeid1997 <ahmadabuzeid1997@gmail.com>
2020-12-23 15:04:32 +0200

    v

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-
+var name;
+name =prompt ('please inter your name');
+alert ('hello '+ name +' weclom to our web site');
+var time;
+time = prompt('what is the time in your country?')
+if (time<12){time='am'}else{time='pm'}
+var x
+x = confirm ("you want to turn on notification for our website?")
+console.log(x)
+if (x == true){alert('thank you')}else{alert('no thanks,git out')}
