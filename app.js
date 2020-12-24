5dd2b8ea2ef5f8ba1380ac512d34fba1770ce6b1
AhmadAbuZeid1997 <ahmadabuzeid1997@gmail.com>
2020-12-24 21:46:57 +0200

    r

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ var x
 x = confirm ("you want to turn on notification for our website?")
 console.log(x)
 if (x == true) {alert('thank you')} else {alert('no thanks,git out')}
+
+var age = prompt ('please inter your age')
+while (age === '') {age = prompt ('please inter your age')}
+console.log(age)
+var y = prompt ('how many stars can you give for this web page please inter a value from 1 to 10')
+
+for (let z=1 ; z <= y ; z++) {
+    document.write( z +"<p>    * </p>");}
