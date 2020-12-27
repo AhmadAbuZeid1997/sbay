eed765321b96d28eb58282135bf850e285badaa6
AhmadAbuZeid1997 <ahmadabuzeid1997@gmail.com>
2020-12-27 15:47:33 +0200

    r

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-var name;
-name =prompt ('please inter your name');
-alert ('hello '+ name +' weclom to our web site');
-var time;
-time = prompt('what is the time in your country?')
-if (time<12){time='am'}else{time='pm'}
-var x
-x = confirm ("you want to turn on notification for our website?")
-console.log(x)
-if (x == true) {alert('thank you')} else {alert('no thanks,git out')}
+var a
+function ask(message)
+{ a= prompt(message)
+while (a===''||a===null||a===undefined) {a= prompt(message)
+    
+}
+}
+ask('inter your name please')
+var z=a
+ask('inter your age please')
+var x=a
+alert('hello '+z+' your age is '+x)
+ask('what is your time now?')
+var y=a
+if (y>12) {y='pm'
+    
+} else {y='am'
+    
+}
+document.write("<p> "+y+" </p>")
+ask('how many stars we deserve (1-5)?')
+var v=a
+for (let i = 0; i < v; i++) {
+    document.write("<p> * </p>");
+    
+}
 
-var age = prompt ('please inter your age')
-while (age === '') {age = prompt ('please inter your age')}
-console.log(age)
-var y = prompt ('how many stars can you give for this web page please inter a value from 1 to 10')
-
-for (let z=1 ; z <= y ; z++) {
-    document.write( z +"<p>    * </p>");}
